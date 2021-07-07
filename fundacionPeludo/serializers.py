d8399d299bfd4f398a2e3bab1b9547801a9b555b
myeber <81203831+myeber@users.noreply.github.com>
2021-07-06 20:02:39 -0400

    Delete serializers.py

--- fundacionPeludo/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import fields, serializers
-from appPeludo.models import Mascotas
-
-class MascotasSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Mascotas
-		fields = ['id','anios','nombre','especie','vacunado','codigo']
