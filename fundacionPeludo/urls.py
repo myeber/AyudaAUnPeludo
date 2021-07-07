9e026ae305ba9e1cf2284a2c02764fad91bfd061
myeber <81203831+myeber@users.noreply.github.com>
2021-07-06 20:02:50 -0400

    Delete urls.py

--- fundacionPeludo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_mascota.views import lista_mascotas, detalle_mascota
-from rest_mascota.viewsLogin import login
-
-urlpatterns = [
-    path('lista_mascotas', lista_mascotas, name="lista_mascotas"),
-    path('detalle_mascota/<str:codigo>/', detalle_mascota, name="detalle_mascota"),
-    path('login', login, name="login"),
-]
